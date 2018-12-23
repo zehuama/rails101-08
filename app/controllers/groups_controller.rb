b1ec50d7bfe300bbfafaa70eaa9dab81163fffc7
javiel <zehua046@gmail.com>
2018-12-23 21:38:40 +0800

    add post relation with group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,7 +19,26 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
-  
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "修改讨论版成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "delete seccess"
+  end
+
   private
 
   def group_params
