f32d8861c744aa957799e25e697105f2fcafd6d8
javiel <zehua046@gmail.com>
2018-12-24 16:14:32 +0800

    install devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_group
-  
+
   def new
     @post = Post.new
   end
@@ -45,3 +45,50 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
 end
+# class PostsController < ApplicationController
+#   before_action :find_group
+#
+#   def new
+#     @post = Post.new
+#   end
+#
+#   def create
+#     @post = Post.new(post_params)
+#     @post.group = @group
+#     if @post.save
+#       redirect_to group_path(@group), notice: "新增文章成功！"
+#     else
+#       render :new
+#     end
+#   end
+#
+#   def edit
+#     @post = Post.find(params[:id])
+#   end
+#
+#   def update
+#     @post = Post.find(params[:id])
+#     @post.group = @group
+#     if @post.update(post_params)
+#       redirect_to group_path(@group), notice: "修改文章成功！"
+#     else
+#       render :edit
+#     end
+#   end
+#
+#   def destroy
+#     @post = Post.find(params[:id])
+#     @post.destroy
+#     redirect_to group_path(@group), alert: "文章已删除"
+#   end
+#
+#   private
+#
+#   def find_group
+#     @group = Group.find(params[:group_id])
+#   end
+#
+#   def post_params
+#     params.require(:post).permit(:content)
+#   end
+# end
