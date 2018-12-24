1bb0969bdab6a0427ded32c5b30d75a1701c08b2
javiel <zehua046@gmail.com>
2018-12-24 17:33:30 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
     if @group.save
       redirect_to groups_path
     else
@@ -25,10 +26,12 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    @group.user = current_user
   end
 
   def update
     @group = Group.find(params[:id])
+    @group.user = current_user
     if @group.update(group_params)
       redirect_to groups_path, notice: "修改讨论版成功"
     else
@@ -38,6 +41,7 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.find(params[:id])
+    @group.user = current_user
     @group.destroy
     redirect_to groups_path, alert: "delete seccess"
   end
